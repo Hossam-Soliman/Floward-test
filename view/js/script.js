5aed72fd794dde78d3d0b9b367e95913d1d12950
unknown <eng.hossamsoliman@gmail.com>
2020-09-14 18:09:56 +0300

    fetch weather data

--- view/js/script.js
+++ view/js/script.js
@@ -1,40 +1,42 @@
-function Weather() {
-}
+function Weather() {}
 
-Weather.prototype.fetchResults = function(val) {
-  
-}
+Weather.prototype.fetchResults = async function (val) {
+  try {
+    const res = await fetch(
+        `https://jsonmock.hackerrank.com/api/weather?name=${val}`
+      ),
+      data = await res.json();
+    console.log("mango", val, data);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-Weather.prototype.onKeyup = function(e) {
-  
-}
+Weather.prototype.onKeyup = function (e) {
+  setTimeout(() => {
+    this.fetchResults(e.target.value);
+  }, 1000);
+};
 
-Weather.prototype.updatecitySelect = function(results) {
-  
-}
+Weather.prototype.updatecitySelect = function (results) {};
 
-Weather.prototype.updateSuggestions = function() {
-  
-}
+Weather.prototype.updateSuggestions = function () {};
 
-Weather.prototype.reset = function() {
-  
-}
+Weather.prototype.reset = function () {};
 
-Weather.prototype.init = function() {
+Weather.prototype.init = function () {
   this.timer = null;
   this.weatherResults = [];
-  this.$city = document.getElementById('city');
-  this.$suggestions = document.getElementById('suggestions');
-  this.$selectedInfo = document.getElementById('selectedCityInfo');
-  this.$selectedCity = document.getElementById('selectedCity');
-  this.$selctedWeather = document.getElementById('selctedWeather');
-  this.$selectedStatus = document.getElementById('selectedStatus');
-  this.$resetBtn = document.getElementById('resetBtn');
-  this.$city.addEventListener('keyup', this.onKeyup.bind(this));
-  this.$resetBtn.addEventListener('click', this.reset.bind(this));
-}
+  this.$city = document.getElementById("city");
+  this.$suggestions = document.getElementById("suggestions");
+  this.$selectedInfo = document.getElementById("selectedCityInfo");
+  this.$selectedCity = document.getElementById("selectedCity");
+  this.$selctedWeather = document.getElementById("selctedWeather");
+  this.$selectedStatus = document.getElementById("selectedStatus");
+  this.$resetBtn = document.getElementById("resetBtn");
+  this.$city.addEventListener("keyup", this.onKeyup.bind(this));
+  this.$resetBtn.addEventListener("click", this.reset.bind(this));
+};
 
 var weatherApp = new Weather();
 weatherApp.init();
-
